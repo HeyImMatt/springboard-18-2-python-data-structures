1e0fa34cb2340a553faacbe2d8dfcdbc9c3c2313
heyimmatt <mrmcfarland@gmail.com>
2020-07-15 12:14:07 -0700

    Solve exercise 32

--- two_list_dictionary.py
+++ two_list_dictionary.py
@@ -14,4 +14,10 @@ def two_list_dictionary(keys, values):
 
         >>> two_list_dictionary(['a', 'b', 'c'], [1, 2, 3, 4])
         {'a': 1, 'b': 2, 'c': 3}
-   """
+    """
+    result = {}
+
+    for idx, el in enumerate(keys):
+        result[el] = values[idx] if idx < len(values) else None
+
+    return result
