4190a3af38b07777a49a8c9d865e1177274ad772
heyimmatt <mrmcfarland@gmail.com>
2020-07-15 12:52:01 -0700

    Solve exercise 38

--- min_max_key_in_dictionary.py
+++ min_max_key_in_dictionary.py
@@ -9,3 +9,7 @@ def min_max_keys(d):
         >>> min_max_keys({"apple": "red", "cherry": "red", "berry": "blue"})
         ('apple', 'cherry')
     """
+    keys_list = list(d.keys())
+    keys_list.sort()
+
+    return (keys_list[0], keys_list[-1])
